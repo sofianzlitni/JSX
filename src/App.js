583884c9a5800b8770d1a56eb45789d3a920359c
sofianzlitni <sofienzlitni@gmail.com>
2022-12-22 15:48:57 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import Project from './component/Project';
+
+
+
+function App() {
+  return (
+    <div>
+    <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:ital,wght@1,900&display=swap');
+    </style>
+<Project/> 
+    </div>
+
+  );
+}
+
+export default App;
